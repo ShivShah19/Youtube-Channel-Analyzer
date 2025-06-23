52c5a4df7e87d2c8d4e6d54541bfb6686a8c0e25
Shiv Shah <102762513+ShivShah19@users.noreply.github.com>
2025-06-24 02:55:50 +0530

    Update Chart.jsx

--- src/Components/Chart.jsx
+++ src/Components/Chart.jsx
@@ -41,10 +41,12 @@ export const Chart = ({ data, type, icon }) => {
                         <YAxis
                             stroke="#ccc"
                             tickFormatter={(value) => {
+                                if (value >= 1_000_000_000) return `${(value / 1_000_000_000).toFixed(1)}B`;
                                 if (value >= 1_000_000) return `${(value / 1_000_000).toFixed(1)}M`;
                                 if (value >= 1_000) return `${(value / 1_000).toFixed(1)}K`;
                                 return value;
                             }}
+
                         />
 
                         <Tooltip
